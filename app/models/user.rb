ee2721f3155332dff49af2e716a79ad63b440e73
Natasha Ho <natashaho1@gmail.com>
2019-07-12 10:35:15 +0800

    added post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+  # validates :username, presence: true
+  # validates :name, presence: true
 end
