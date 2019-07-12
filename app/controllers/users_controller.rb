5e53fd8dc879c8a93e8e91882cbda7a42bd8a9ec
Natasha Ho <natashaho1@gmail.com>
2019-07-12 10:35:05 +0800

    users actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+    before_action :set_user
     def show
     end
+
+    def edit
+        unless @user == current_user
+            flash[:danger] = "You can only edit your own profile"
+            redirect_to user_path(@user)
+        end
+    end
+
+    def update
+        if @user.update(user_params)
+            flash[:success] = "User is successfully updated"
+            redirect_to current_user
+        else
+            flash.now[:danger] = "Something went wrong. Please try again"
+        end
+    end
+
+    private
+        def set_user
+            @user = User.find(params[:id])
+        end
+
+        def user_params
+            params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
+        end
 end
