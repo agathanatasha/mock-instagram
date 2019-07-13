3b6f39c49a06bc625004282dbac10142c54c19c2
Natasha Ho <natashaho1@gmail.com>
2019-07-14 00:21:06 +0800

    show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,19 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show]
+    
     def create
         Post.create(post_params)
         redirect_to root_path
-      end
+    end
+
+    def show
+    end
+
     private
+        def set_post
+            @post = Post.find(params[:id])
+        end
+
         def post_params
             params.require(:post).permit(:description, :image, :user_id)
         end
