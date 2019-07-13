206a8e397c72226ace79434719ca525ed0a6a747
Natasha Ho <natashaho1@gmail.com>
2019-07-13 10:10:26 +0800

    fix pluralize bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-    def pluralize_without_count(count, noun, text = nil)
-        if count != 0
-            count == 1 ? "#{text}#{noun}" : "#{text}#{noun.pluralize}"
-        end
-    end
 end
