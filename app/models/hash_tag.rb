dd8922c3e8f6fa285f101c490e96e3d33480b553
Natasha Ho <natashaho1@gmail.com>
2019-07-16 23:23:03 +0800

    create hashtags after creating posts

--- app/models/hash_tag.rb
+++ app/models/hash_tag.rb
@@ -1,4 +1,5 @@
 class HashTag < ApplicationRecord
     has_many :post_hash_tags
     has_many :posts, through: :post_hash_tags
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
 
   has_many :post_hash_tags
   has_many :hash_tags, through: :post_hash_tags
+  after_commit :create_hash_tags, on: :create
 
   has_one_attached :image
 
@@ -10,5 +11,15 @@ class Post < ApplicationRecord
 
   def image_presence
     errors.add(:image, "can't be blank") unless image.attached?  
+  end
+
+  def create_hash_tags
+    extract_name_hash_tags.each do |name|
+      hash_tags.create(name: name)
+    end
+  end
+
+  def extract_hash_tag_names
+    description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "").downcase}
   end
 end
